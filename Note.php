f1b8b03294dfbbb4d78a9ee3bd2db0604e26726a
Fatimatou <dieynahane@yahoo.fr>
2017-02-28 20:26:45 +0100

    Create Note.php

---
+++ Note.php
@@ -0,0 +1,27 @@
+<?php
+  /** model pour gerer les ajout DE NOTE
+  /** une note concerne qu'un seul élève
+  * 
+  */
+  App::uses('AppModel','Model');
+
+  class Note extends AppModel{
+  	public $useTable= 'notes';
+  	// nom de modèle
+  	var $nam= 'Note';
+  	// je définis la relations entre les deux tables eleve et note
+  	public $belongsTo = array(
+  		'Eleve'=> array(
+  			'ClassName'=>'Eleve',
+  			'foreignKey'=>'eleve_id',
+  			)
+  		);
+  	//rglès de validation des données pour le formulaire ajout
+  	public $validate= array(
+  		'nommatiere'=>array('rule'=>array('custom', '#([a-zA-Z])+#'),'required'=>true,'allowEmpty'=>false,'message'=>'Merci de remplir les bonnes valeurs pour le champs matière svp!'),
+  		'grade'=>array('rule'=>array('custom','#([0-1]{1}[0-9])#','required'=>true,'allowEmpty'=>false,'message'=>'Veuillez remplir le champs Note svp!')
+  			)
+  		);
+
+  }
+?>
